6d5a8d4a6db7c805c0d9d73c69d709a0545d413f
inass090 <nena0009000@gmail.com>
2025-10-21 02:24:22 +0800

    Create converting from Fahrenheit to celisius.cpp

---
+++ converting from Fahrenheit to celisius.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+double Fahrenheit;
+
+cout<<"Enter the Temprature in Fahrenheit: ";
+cin>>Fahrenheit;
+double Celsius = 5.0/9*(Fahrenheit-32);
+cout<<Fahrenheit <<" when converted to Celsius is: "<< Celsius ;
+
+
+    return 0;
+}
